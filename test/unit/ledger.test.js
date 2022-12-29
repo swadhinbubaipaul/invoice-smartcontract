2f41b2309ef070419b7a62cbab403bd334453aaf
swadhinbubaipaul <swadhinpaul159@gmail.com>
2022-12-29 21:47:43 +0530

    more tests added

--- test/unit/ledger.test.js
+++ test/unit/ledger.test.js
@@ -1,36 +1,89 @@
-const { assert } = require("chai");
-const { network, deployments, ethers } = require("hardhat");
+const { assert, expect } = require("chai");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { network, ethers } = require("hardhat");
 const { developmentChains } = require("../../helper-hardhat-config");
+
+const BUYERPAN = "BUYERPAN";
+const SELLERPAN = "SELLERPAN";
+const INVOICEAMOUNT = 1000;
+const INVOICEDATE = "22-12-22";
+const STATUS = "paid";
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("Ledger Unit Tests", function () {
-      let ledger, ledgerContract;
-      beforeEach(async () => {
-        accounts = await ethers.getSigners(); // could also do with getNamedAccounts
-        deployer = accounts[0];
-        await deployments.fixture(["all"]);
-        ledgerContract = await ethers.getContract("Ledger");
-        ledger = ledgerContract.connect(deployer);
-        const buyerPan = "BUYERPAN";
-        const selerPan = "SELLERPAN";
-        const invoiceAmount = 1000;
-        const invoiceDate = "22-12-22";
-        const status = true;
-        const tx = await ledger.saveInvoice(
-          buyerPan,
-          selerPan,
-          invoiceAmount,
-          invoiceDate,
-          status
-        );
-        await tx.wait(1);
+  : describe("Ledger Contract", function () {
+      async function deployTokenFixture() {
+        [owner] = await ethers.getSigners(); // could also do with getNamedAccounts
+        const Ledger = await ethers.getContractFactory("Ledger");
+        const ledger = await Ledger.deploy();
+        return { ledger };
+      }
+
+      describe("saveInvoice", function () {
+        it("should revert on empty buyerPan", async function () {
+          const { ledger } = await loadFixture(deployTokenFixture);
+
+          await expect(
+            ledger.saveInvoice(
+              "",
+              SELLERPAN,
+              INVOICEAMOUNT,
+              INVOICEDATE,
+              STATUS
+            )
+          ).to.be.revertedWithCustomError(ledger, "EmptyDetailsEntered");
+        });
+
+        it("should revert on empty sellerPan", async function () {
+          const { ledger } = await loadFixture(deployTokenFixture);
+
+          await expect(
+            ledger.saveInvoice(BUYERPAN, "", INVOICEAMOUNT, INVOICEDATE, STATUS)
+          ).to.be.revertedWithCustomError(ledger, "EmptyDetailsEntered");
+        });
+
+        it("should revert on 0 invoice amount", async function () {
+          const { ledger } = await loadFixture(deployTokenFixture);
+
+          await expect(
+            ledger.saveInvoice(BUYERPAN, SELLERPAN, 0, INVOICEDATE, STATUS)
+          ).to.be.revertedWithCustomError(ledger, "EmptyDetailsEntered");
+        });
+
+        it("should revert on empty invoice date", async function () {
+          const { ledger } = await loadFixture(deployTokenFixture);
+
+          await expect(
+            ledger.saveInvoice(BUYERPAN, SELLERPAN, INVOICEAMOUNT, "", STATUS)
+          ).to.be.revertedWithCustomError(ledger, "EmptyDetailsEntered");
+        });
+
+        it("should revert on empty status", async function () {
+          const { ledger } = await loadFixture(deployTokenFixture);
+
+          await expect(
+            ledger.saveInvoice(
+              INVOICEAMOUNT,
+              SELLERPAN,
+              INVOICEAMOUNT,
+              INVOICEDATE,
+              ""
+            )
+          ).to.be.revertedWithCustomError(ledger, "EmptyDetailsEntered");
+        });
       });
 
       describe("getInvoiceIds", function () {
         it("gets invoice ids for a buyerPan", async function () {
-          const buyerPan = "BUYERPAN";
-          let ids = await ledger.getInvoiceIds(buyerPan);
+          const { ledger } = await loadFixture(deployTokenFixture);
+          await ledger.saveInvoice(
+            BUYERPAN,
+            SELLERPAN,
+            INVOICEAMOUNT,
+            INVOICEDATE,
+            STATUS
+          );
+          let ids = await ledger.getInvoiceIds(BUYERPAN);
           console.log("Id's are: ");
           ids = ids.map((id) => id.toNumber());
           assert(ids[0] === 0);
@@ -39,12 +92,8 @@ const { developmentChains } = require("../../helper-hardhat-config");
 
       describe("getInvoiceById", function () {
         it("gets invoice for invoiceId", async function () {
-          const buyerPan = "BUYERPAN";
-          const SELLERPAN = "SELLERPAN";
-          const INVOICEAMOUNT = 1000;
-          const INVOICEDATE = "22-12-22";
-          const STATUS = true;
-          let ids = await ledger.getInvoiceIds(buyerPan);
+          const { ledger } = await loadFixture(deployTokenFixture);
+          let ids = await ledger.getInvoiceIds(BUYERPAN);
           ids = ids.map((id) => id.toNumber());
 
           for (id of ids) {
